144a68791b7fb519e361aeb6a384b87cf6b7c5a6
redbeard339 <coderedbeard339@gmail.com>
2022-04-18 14:46:38 -0500

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    let reversedString = [];
+    for(let i = splitString.length; i >= 0; i--){
+      reversedString.push(splitString[i])
+    }
+    return reversedString.join("");
 };
 
 // Do not edit below this line
