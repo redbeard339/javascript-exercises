36bffc06246bc57754daab841c68f35125f57beb
redbeard339 <coderedbeard339@gmail.com>
2022-04-18 14:31:59 -0500

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let repeatedString = "";
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            repeatedString += string;
+        }
+    } else {
+        return 'ERROR';
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
