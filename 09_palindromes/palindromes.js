7845f5d11fd6394b50bd9e3fb0efa49166936155
redbeard339 <coderedbeard339@gmail.com>
2022-04-19 15:54:29 -0500

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const re = /[\W_]/g;
 
-};
+    const str = string.replace(re, "").toLowerCase();
+    console.log(str);
+    const newStr = str.split("").reverse().join("")
+    console.log(newStr);
+
+    if (newStr === str) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
