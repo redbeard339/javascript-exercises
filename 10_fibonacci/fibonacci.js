9aa774de752ea671ec432ffb185d79fcd8be9418
redbeard339 <coderedbeard339@gmail.com>
2022-04-19 17:34:13 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        let n1 = 0, n2 = 1, nextTerm;
+        for (let i = 1; i <= num; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+        return n1;
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
