9d9e679d5c02ba137d3da6f39f4bbc83e77fd068
redbeard339 <coderedbeard339@gmail.com>
2022-04-19 13:36:11 -0500

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const cel = (fahrenheit - 32) * 5/9;
+  return parseFloat(cel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fah = (celsius * 9/5) + 32;
+  return parseFloat(fah.toFixed(1));
 };
 
 // Do not edit below this line
